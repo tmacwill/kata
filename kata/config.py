d0af5da34c42c6e1781a33ff0e54031f8b616fe1
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2016-05-04 07:48:19 +0000

    update schema module for new config
    refactor the schema module to be compatible with new config schema.
    introduce a new config arg that doesn't connect to the database/cache on
    application startup so the database can be dropped on reset.

--- kata/config.py
+++ kata/config.py
@@ -10,14 +10,21 @@ def app():
 
     return _app
 
-def initialize(config_file):
+def initialize(config_file, bare=False):
     with open(config_file, 'r') as f:
         data = yaml.load(f.read())
 
-        if 'database' in data:
-            import kata.db
-            kata.db.initialize(data['database'])
+        if 'database' in data or 'cache' in data:
+            import kata.schema
+            kata.schema.initialize(data['database'], data['cache'])
+
+        if bare:
+            return
 
         if 'cache' in data:
             import kata.cache
             kata.cache.initialize(data['cache'])
+
+        if 'database' in data:
+            import kata.db
+            kata.db.initialize(data['database'])
--- kata/schema.py
+++ kata/schema.py
@@ -2,17 +2,34 @@ import os
 import natsort
 import redis
 
-import kata.config
+_cache = None
+_database = None
+
+def initialize(database, cache):
+    global _cache
+    global _database
+
+    _cache = cache
+    _database = database
 
 def reset(schema):
-    os.system('dropdb -U %s %s' % (kata.config.database.user, kata.config.database.database))
-    os.system('createdb -U %s %s' % (kata.config.database.user, kata.config.database.database))
+    global _cache
+    global _database
 
-    for file in natsort.natsorted(os.listdir(schema)):
-        print('Applying %s' % file)
-        os.system(
-            'psql -U %s %s < %s > /dev/null' %
-            (kata.config.database.user, kata.config.database.database, schema + '/' + file)
-        )
+    if _database:
+        os.system('dropdb -U %s %s' % (_database['user'], _database['name']))
+        os.system('createdb -U %s %s' % (_database['user'], _database['name']))
 
-    os.system('redis-cli KEYS "%s*" | xargs redis-cli DEL > /dev/null' % kata.config.redis.prefix)
+        for file in natsort.natsorted(os.listdir(schema)):
+            print('Applying %s' % file)
+            os.system(
+                'psql -U %s %s < %s > /dev/null' % (_database['user'], _database['name'], schema + '/' + file)
+            )
+
+    if _cache:
+        for name, data in _cache.items():
+            if data['type'] == 'redis':
+                os.system(
+                    'redis-cli -h %s -p %s KEYS "%s*" | xargs redis-cli DEL > /dev/null' %
+                    (data.get('host', 'localhost'), data.get('port', 6379), data.get('prefix', ''))
+                )
