812928815c41ccf837aa2be53773a4de0415e4eb
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2016-06-21 04:58:10 +0000

    properly get git hash for sentry
    sentry's built-in fetch_git_sha doesn't support mirrors, so do the
    right thing instead.

--- kata/errors.py
+++ kata/errors.py
@@ -1,4 +1,5 @@
 import raven
+import subprocess
 
 _sentry_client = None
 
@@ -8,7 +9,7 @@ def initialize(config):
     if 'sentry' in config:
         kwargs = {'dsn': config['sentry']['dsn']}
         if 'git' in config['sentry']:
-            kwargs['release'] = raven.fetch_git_sha(config['sentry']['git'])
+            kwargs['release'] = _git_commit(config['sentry']['git'])
         elif 'release' in config['sentry']:
             kwargs['release'] = config['sentry']['release']
 
@@ -19,3 +20,10 @@ def handler(exception, request, response, parameters):
         _sentry_client.captureException()
 
     raise exception
+
+def _git_commit(path):
+    return subprocess.check_output(
+        'cd %s ; git log --format="%%H" -1' % path,
+        shell=True,
+        universal_newlines=True
+    )[:-1]
